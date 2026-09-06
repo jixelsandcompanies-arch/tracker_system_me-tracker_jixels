8d918b7723efaec5c7f7c40153a1cfebab8b6b71
Jixels Companies <jixelsandcompanies-arch@users.noreply.github.com>
2026-09-06 23:38:12 +0300

    Prepare customer app version 1.0.1

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Jixels Customer Trackings",
     slug: "jixels-customer-trackings",
-    version: "1.0.0",
+    version: "1.0.1",
     scheme: "jixelscustomertrackings",
 
     orientation: "portrait",
@@ -19,7 +19,7 @@ export default {
 
     android: {
       package: "com.jixelstechnologies.customer",
-      versionCode: 1,
+      versionCode: 3,
       softwareKeyboardLayoutMode: "resize",
 
       adaptiveIcon: {
